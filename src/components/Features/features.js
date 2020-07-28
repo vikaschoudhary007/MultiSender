d453d5ac771c1512216fa38645bce59c4759ae24
vikaschoudhary007 <vikas choudhary572001@gmail.com>
2020-07-28 16:41:12 +0530

    token functionality added

--- src/components/Features/features.js
+++ src/components/Features/features.js
@@ -3,95 +3,126 @@ import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 import { AvForm, AvField } from 'availity-reactstrap-validation';
 import ReactFileReader from "react-file-reader";
-import getWeb3 from "../../getWeb3";
 import web3 from "web3";
+import ERC20Abi from "../../contracts/ERC20ABI.json";
 
-var abi = require('human-standard-token-abi');
 
 export default function Features(props) {
 
-    const [addresses, setAddresses] = useState([]);
-    const [balances, setBalances] = useState([]);
-    const [tokenAddress, setTokenAddress] = useState("");
+    const [etherAddresses, setEtherAddresses] = useState([]);
+    const [etherBalances, setEtherBalances] = useState([]);
+    const [totalEtherAmount, setTotalEtherAmount] = useState("");
+
+    const [ERC20Address, setERC20Address] = useState("");
+    const [tokenAddresses, setTokenAddresses] = useState([]);
+    const [tokenBalances, setTokenBalances] = useState([])
     const [decimal,setDecimal] = useState(18);
-    const [totalAmount, setTotalAmount] = useState("");
     
+    const uploadCsvForEther = (files) => {
 
-    const handleFiles = (files) => {
-
-        setAddresses([]);
-        setBalances([])
+        setEtherAddresses([]);
+        setEtherBalances([])
 
         var reader = new FileReader();
         reader.onload = function(e) {
-            // alert(reader.result)
 
             let lines = reader.result.split('\n');
 
             lines.forEach(element => {
                 var temp = element.split(",");
-                addresses.push(temp[0]);
-                balances.push(temp[1]);              
-        });
-            console.log(addresses);
-            console.log(balances)
+                etherAddresses.push(temp[0]);
+                etherBalances.push((temp[1]*1000000000000000000).toString());              
+            });
+            console.log(etherAddresses);
+            console.log(etherBalances)
+            setEtherAddresses(etherAddresses);
+            setEtherBalances(etherBalances);    
         }
-
         reader.readAsText(files[0]);
     }
 
-    const handleClick = async()  => {
-        console.log(addresses);
-        console.log(balances);
+    const uploadCsvForToken = (files) => {
 
-    
-        const result = await props.multiSender.methods.mutiSendETHWithDifferentValue(
-            ["0xad7b72b4F775C24fD31a921490248A4Ea382E7fA","0x435a4787Af28293934161F5e22f4F7368B10D2Af"],
-            ["1000000000000000","1000000000000000"]
-            ).send({from: props.account, value:web3.utils.toWei(totalAmount, 'ether')})
-        
-        console.log(result)
-        
-         
+        setTokenAddresses([]);
+        setTokenBalances([])
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+
+            let lines = reader.result.split('\n');
+
+            lines.forEach(element => {
+                var temp = element.split(",");
+                tokenAddresses.push(temp[0]);
+                tokenBalances.push((temp[1]*1000000000000000000).toString());              
+            });
+            console.log(tokenAddresses);
+            console.log(tokenBalances)
+            setTokenAddresses(tokenAddresses);
+            setTokenBalances(tokenBalances);    
+        }
+        reader.readAsText(files[0]);
     }
 
-    // const handleInitialize = async() => {
-    //     const web3 = await getWeb3();
-    //     const tokenContract = new web3.eth.contract();
-    //     console.log(tokenContract)
-    // }
+    const initialize = async() => {
+        const Web3 = new web3(web3.givenProvider);
+        const accounts = await Web3.eth.getAccounts();
+        console.log(accounts)
 
-  
+        const tokenContract = new Web3.eth.Contract(ERC20Abi,ERC20Address)
+
+        console.log(tokenContract);
+
+        const result = await tokenContract.methods.approve("0xf6f7ba71a1EA85CE09063180E6F6919C50EE1056","1000000000000000000000").send({from:props.account});
+        console.log(result)
+
+    }
+
+
+    const sendEther = async()  => {
+      
+        const result = await props.multiSender.methods.mutiSendETHWithDifferentValue(
+           etherAddresses,
+           etherBalances
+            ).send({from: props.account, value:web3.utils.toWei(totalEtherAmount, 'ether')})
+        
+        console.log(result)       
+    }
+
+    const sendToken = async() => {
+        console.log(ERC20Address)
+       
+        const result = await props.multiSender.methods.mutiSendCoinWithDifferentValue(
+            ERC20Address,
+            tokenAddresses,
+            tokenBalances
+            ).send({from: props.account, value:web3.utils.toWei('0.11', "ether")});
+
+        console.log(result)
+    }
+
         return (
             <React.Fragment>
                 <section className="section" id="features">
-                <div style={{textAlign:"center"}}><h2>USE MULTISENDER</h2></div>
+                {/* <div style={{textAlign:"center"}}><h2>USE MULTISENDER</h2></div> */}
                     <Container>  
-                    <div ><h2>Transfer Ether</h2></div>
+                    
                         <Row className="align-items-center">
                             <Col lg="5" className="order-2 order-lg-1">
+                                <div ><h2>Transfer Ether</h2></div>
                                 <div className="features-box mt-5 mt-lg-0">
-                                    {/* <h3>Use MultiSender</h3> */}
-                                    {/* <p className="text-muted web-desc">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                                    <ul className="text-muted list-unstyled margin-t-30 features-item-list">
-                                        <li className="">We put a lot of effort in design.</li>
-                                        <li className="">The most important ingredient of successful website.</li>
-                                        <li className="">Submit Your Orgnization.</li>
-                                    </ul> */}
-
-
                                 <div className="custom-form mt-4 pt-4">
                                     <div id="message"></div>
                                     <AvForm name="contact-form" id="contact-form">
                                         <Row>
                                             <Col lg="6">
-                                                <AvField type="text" className="form-group mt-2" name="name" id="name" placeholder="Total Amount*" required
+                                                <AvField type="text" className="form-group mt-2" name="name" id="name" placeholder="Total Amount in ETH*" required
                                                     errorMessage=""
                                                     validate={{
                                                         required: {value: true, errorMessage: "This field is mandatory"},
                                                       }}
-                                                      value={tokenAddress}
-                                                      onChange={(event) => setTotalAmount(event.target.value)}
+                                                      value={totalEtherAmount}
+                                                      onChange={(event) => setTotalEtherAmount(event.target.value)}
                                                 />
                                                
                                             </Col>
@@ -120,25 +151,24 @@ export default function Features(props) {
                                         <Row>
                                             <Col lg="12">
                                                 <div className="form-group mt-2">
-                                                    <textarea name="comments" id="comments"  rows="4" className="form-control" placeholder="List of Addresses in CSV"></textarea>
+                                                    <textarea readOnly="true" name="comments" id="comments"  rows="4" className="form-control" placeholder="List of Addresses in CSV"></textarea>
                                                 </div>
                                             </Col>
                                         </Row>
                                         <Row>
-                                            <Col lg="12" className="text-left">
-                                                <ReactFileReader fileTypes={".csv"} multipleFiles={false} handleFiles={handleFiles}>
+                                            <Col lg="6" className="text-left">
+                                                <ReactFileReader fileTypes={".csv"} multipleFiles={false} handleFiles={uploadCsvForEther}>
                                                     <button  className="submitBnt btn btn-custom">Upload CSV file </button>
                                                     <div id="simple-msg"></div>
                                                 </ReactFileReader>
                                                 
                                             </Col>
-                                        </Row>
-                                        <Row>
-                                            <Col lg="12" className="text-right">
-                                                <button  className="submitBnt btn btn-custom" onClick={handleClick}>Send <i className="mdi mdi-arrow-right"></i></button>
+                                            <Col lg="6" className="text-right">
+                                                <button  className="submitBnt btn btn-custom" onClick={sendEther}>Send <i className="mdi mdi-arrow-right"></i></button>
                                                 <div id="simple-msg"></div>
                                             </Col>
                                         </Row>
+                                        
                                     </AvForm>
                                     
 
@@ -146,10 +176,66 @@ export default function Features(props) {
                                 </div>
                                 </div>
                             </Col>
-                            <Col lg={{size: 7, order: 2, }} xs={{order : 1}}>
-                                <div className="features-img mx-auto mr-lg-0">
-                                    <img src="assets/images/growth-analytics.svg" alt="macbook" className="img-fluid" />
+
+                            <Col lg={{size: 1, order: 3}} xs={{order:2}}></Col>
+
+                            <Col lg={{size: 6, order: 3, }} xs={{order : 3}} className="order-2 order-lg-1">
+                                <div ><h2>Transfer Tokens</h2></div>
+                                <div className="features-box mt-5 mt-lg-0">
+                                    <div className="custom-form mt-4 pt-4">
+                                        <AvForm>
+                                            <Row>
+                                                <Col lg="8">
+                                                    <AvField type="text" className="form-group mt-2" name="name" id="name" placeholder="Token Address*" required
+                                                        errorMessage=""
+                                                        validate={{
+                                                            required: {value: true, errorMessage: "This field is mandatory"},
+                                                        }}
+                                                        value={ERC20Address}
+                                                        onChange={(event) => setERC20Address(event.target.value)}
+                                                    />
+                                                </Col>
+                                                <Col lg="4">
+                                                    <AvField type="text" className="form-group mt-2" name="name" id="name" placeholder="Decimal*" required
+                                                        errorMessage=""
+                                                        validate={{
+                                                            required: {value: true, errorMessage: "This field is mandatory"},
+                                                        }}
+                                                        value={decimal}
+                                                        onChange={(event) => setDecimal(event.target.value)}
+                                                    />
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col lg="12">
+                                                    <div className="form-group mt-2">
+                                                        <textarea readOnly="true" name="comments" id="comments"  rows="4" className="form-control" placeholder="List of Addresses in CSV"></textarea>
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col lg="4" className="text-left">
+                                                    <button  className="submitBnt btn btn-custom" onClick={initialize}>Initialize</button>
+                                                    <div id="simple-msg"></div>    
+                                                </Col>
+                                                <Col lg="4" className="text-center">
+                                                    <ReactFileReader fileTypes={".csv"} multipleFiles={false} handleFiles={uploadCsvForToken}>
+                                                        <button  className="submitBnt btn btn-custom">Upload CSV file </button>
+                                                        <div id="simple-msg"></div>
+                                                    </ReactFileReader>                                                  
+                                                </Col>
+                                                <Col lg="4" className="text-right">
+                                                    <button  className="submitBnt btn btn-custom" onClick={sendToken}>Send <i className="mdi mdi-arrow-right"></i></button>
+                                                    <div id="simple-msg"></div>
+                                                </Col>
+                                            </Row>
+                                        </AvForm>
+                                    </div>
                                 </div>
+                                 {/* <div className="features-img mx-auto mr-lg-0">
+                                    <img src="assets/images/growth-analytics.svg" alt="macbook" className="img-fluid" />
+                                </div> */}
+                                
                             </Col>
                         </Row>
                     </Container>
